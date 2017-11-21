f97e82a1c6670229d8131991c6b903861e909c8c
bigjerm79 <jjeremy.kay@gmail.com>
2017-11-21 16:12:19 -0600

    412

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,111 @@
 
 
-$("#G1").html("<img src=assets/images/cartman.png />");
-$("#G2").html("<img src=assets/images/kyle.png />");
-$("#G3").html("<img src=assets/images/kenny.png />");
-$("#G4").html("<img src=assets/images/stan.png />");
+$("#SP1").html("<img src=assets/images/cartman.png />");
+$("#SP2").html("<img src=assets/images/kyle.png />");
+$("#SP3").html("<img src=assets/images/kenny.png />");
+$("#SP4").html("<img src=assets/images/stan.png />");
 
 
-var num = Math.floor((Math.random() * 100) + 1);
+var targetNum = Math.floor((Math.random() * 100) + 1);
 
 
-document.getElementById("targetScore").innerHTML = "Random Number " + num;
+document.getElementById("targetScore").innerHTML = "Random Number " + targetNum;
+
+
+var wins = 0;
+var losses = 0;
+var urscore =0;
+var g1Val;
+var g2Val;
+var g3Val;
+var g4Val;
+
+
+$(document)
+      // alert('button clicked');
+
+    var G1 = Math.floor(Math.random() * (10-1) + 1);
+    var G2 = Math.floor(Math.random() * (10-1) + 1);
+    var G3 = Math.floor(Math.random() * (10-1) + 1);
+    var G4 = Math.floor(Math.random() * (10-1) + 1);
+
+    console.log(G1);
+    console.log(G2);
+    console.log(G3);
+    console.log(G4);
+
+$('.children').on("click", function() {
+	var who = $(this).attr('id')
+
+	if (who === "SP1") {
+		urscore = urscore + G1;
+
+  $("#score").html("Your Score " + urscore);	
+
+		console.log(urscore);
+		
+     } else if (who === "SP2") {
+     	urscore = urscore + G2;
+  
+  $("#score").html("Your Score " + urscore);
+
+     } else if (who === "SP3") {
+     	urscore = urscore + G3;
+  
+  $("#score").html("Your Score " + urscore);
+
+     } else if (who === "SP4") {
+     	urscore = urscore + G4;
+  
+  $("#score").html("Your Score " + urscore);
+
+    }
+})
+	
+
+
+       
+
+    
+    
 
 
 
 // cartman G1
 
-var g1Num = Math.floor(Math.random() * ((10-1)+1) + 1);
+// var g1Num = Math.floor(Math.random() * ((10-1)+1) + 1);
 
-
-document.getElementById("score").innerHTML = "Your Score " + g1Num;
+// console.log(g1Num)
+// document.getElementById("score").innerHTML = "Your Score " + g1Num;
 
 
 // kyle G2
 
-var g2Num = Math.floor(Math.random() * ((10-1)+1) + 1);
+// var g2Num = Math.floor(Math.random() * ((10-1)+1) + 1);
 
 
-document.getElementById("score").innerHTML = "Your Score " + g2Num;
+// document.getElementById("score").innerHTML = "Your Score " + g2Num;
 
 // kenny G3
 
-var g3Num = Math.floor(Math.random() * ((10-1)+1) + 1);
+// var g3Num = Math.floor(Math.random() * ((10-1)+1) + 1);
 
 
-document.getElementById("score").innerHTML = "Your Score " + g3Num;
+// document.getElementById("score").innerHTML = "Your Score " + g3Num;
 
 // stan G4
 
-var g4Num = Math.floor(Math.random() * ((10-1)+1) + 1);
+// var g4Num = Math.floor(Math.random() * ((10-1)+1) + 1);
 
 
-document.getElementById("score").innerHTML = "Your Score " + g4Num;
+// document.getElementById("score").innerHTML = "Your Score " + g4Num;
 
 
+ 
+
+
+
+// });
 
 
 
